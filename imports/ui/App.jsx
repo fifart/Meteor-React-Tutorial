aa278cd54160340f2043485a1c10ca90b570329c
TASSOS P <fifart@gmail.com>
2022-08-30 12:12:17 +0300

    Filter added

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  const [hideCompleted, setHideCompleted] = useState(false);
+  const hideCompleteFilter = {isChecked: { $ne: true }};
+  const tasks = useTracker(() => TasksCollection.find(hideCompleted ? hideCompleteFilter : {}, { sort: { createdAt: -1 } }).fetch());
   const toggleChecked = ({ _id, isChecked }) => {
     TasksCollection.update(_id, {
       $set: {
@@ -17,11 +19,24 @@ export const App = () => {
 
 
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>ToDo List!</h1>
+          </div>
+        </div>
+      </header>
+      <div className="main">
       <TaskForm />
-      <ul>
+      <div className="filter">
+         <button onClick={() => setHideCompleted(!hideCompleted)}>
+           {hideCompleted ? 'Show All' : 'Hide Completed'}
+         </button>
+       </div>
+      <ul className="tasks">
       { tasks.map(task => <Task key={ task._id } task={ task } onCheckboxClick={toggleChecked} onDeleteClick={deleteTask} />) }      </ul>
+    </div>
     </div>
   );
 };
